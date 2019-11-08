df7cffda2f78a8837e49c72293be560f5aa54f66
Nebulino <i.am.nebulino@gmail.com>
2019-11-08 11:49:10 +0100

    Creating exercise-5-eperators

---
+++ exercise-5-operators/cmake-build-debug/CMakeFiles/exercise_5_operators.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exercise_5_operators.dir/main.cpp.o"
+  "exercise_5_operators.pdb"
+  "exercise_5_operators"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exercise_5_operators.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ exercise-5-operators/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+/* Operators
+ * 1 Operator
+ * !@ -@ @[] *@
+ *
+ * 2 Operators
+ * @+@ @=@ << && > <=
+ *
+ * Can't be re-defined...
+ * . & (1 operator - for address)
+ *
+ * OP= OP[] OP() need to be defined inside the custom value
+ * Only inside the class
+ *
+ */
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
